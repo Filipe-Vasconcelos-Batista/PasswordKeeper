ab124a1f30016c83d06ef851e6f942ae7d9d73ea
Filipe Batista <filipevbatista@live.com>
2024-09-17 08:13:18 +0200

    feat:add password page

--- App/src/Controller/PasswordController.php
+++ App/src/Controller/PasswordController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Password;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class PasswordController extends AbstractController
 {
     #[Route('/password', name: 'app_password')]
-    public function index(): Response
+    public function index(EntityManagerInterface $manager): Response
     {
+        $password=$manager->getRepository(Password::class)->findAll();
         return $this->render('password/index.html.twig', [
-            'controller_name' => 'PasswordController',
+            'password'=> $password
         ]);
     }
 }
