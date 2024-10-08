606d42164cddac474bbf60b545222fa583d18a9f
Filipe Batista <filipevbatista@live.com>
2024-10-08 16:57:38 +0200

    style:Restructed the insert/generate password page

--- App/src/Form/PasswordType.php
+++ App/src/Form/PasswordType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Password;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,16 +16,35 @@ class PasswordType extends AbstractType
     {
         parent::buildForm($builder, $options);
         $builder
-            ->add('password')
+            ->add('password', TextType::class,[
+                'label' => false,
+                'attr'=>[
+                    'class'=>'form-control',
+                ]
+            ])
             ->add('securityLevel', ChoiceType::class,[
+                'label' => false,
                 'choices'=>[
                     'Low'=>'low',
                     'Medium'=>'medium',
                     'High'=>'high',
                 ],
+                'attr'=>[
+                    'class'=>'form-select'
+                ]
             ])
-            ->add('local')
-            ->add('description')
+            ->add('local',TextType::class,[
+                'label' => false,
+                'attr'=>[
+                    'class'=>'form-control',
+                ]
+            ])
+            ->add('description',TextareaType::class,[
+                'label' => false,
+                'attr'=>[
+                    'class'=>'form-control',
+                ]
+            ])
         ;
     }
 
